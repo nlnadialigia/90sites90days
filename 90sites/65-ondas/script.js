752b18d260f054108e6ed013ce6cd5627e50b764
hedenicapedrosa <hedenica@hotmail.com>
2020-11-06 17:16:54 -0300

    added wave sounds, changed background styles and paused sound

--- 90sites/65-ondas/script.js
+++ 90sites/65-ondas/script.js
@@ -2,20 +2,23 @@ const container = document.getElementById('container');
 const circlesArr = [];
 const soundsArr = [];
 const tracks = [
-	'/sounds/1.wav',
-	'/sounds/2.wav'
+	'/sounds/waves-sound-1.wav',
+	'/sounds/waves-sound-2.mp3',
+	'/sounds/waves-sound-3.mp3'
 ]
 let rows = 15;
 let cols = 15;
 
-for(let i=0; i<cols; i++) {
+for (let i = 0; i < cols; i++) {
 	circlesArr[i] = [];
 	soundsArr[i] = []
-	for(let j=0; j<rows; j++) {
+
+	for (let j = 0; j < rows; j++) {
 		const circle = document.createElement('div');
 		circle.classList.add('circle');
 		container.appendChild(circle);
 		circlesArr[i].push(circle);	
+
 		const sound = document.createElement('audio');
 		sound.classList.add('sound')
 		circle.appendChild(sound)
@@ -58,4 +61,8 @@ function playAudio() {
 		audio.src = track;
 		audio.play()
 	}, 0)
+
+	setTimeout(() => {
+		audio.pause()
+	}, 3550)
 }
