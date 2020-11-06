d73ad2618305946ae02a40fc05c302907df80bf9
Nadia Ligia <nlnadialigia@gmail.com>
2020-11-06 16:58:07 -0300

    add color

--- 90sites/65-ondas/script.js
+++ 90sites/65-ondas/script.js
@@ -3,8 +3,36 @@ const circlesArr = [];
 const soundsArr = [];
 const tracks = [
 	'/sounds/1.wav',
-	'/sounds/2.wav'
-]
+	'/sounds/2.wav',
+	'/sounds/3.wav',
+	'/sounds/4.wav',
+	'/sounds/5.wav',
+	'/sounds/6.wav',
+	'/sounds/7.wav',
+	'/sounds/8.wav',
+	'/sounds/9.wav',
+	'/sounds/10.wav',
+	'/sounds/11.wav',
+	'/sounds/12.wav',
+	'/sounds/13.wav',
+	'/sounds/14.wav',
+	'/sounds/15.wav',
+	'/sounds/16.wav',
+	'/sounds/17.wav',
+	'/sounds/18.wav',
+	'/sounds/19.wav',
+	'/sounds/20.wav',
+	'/sounds/21.wav',
+	'/sounds/22.wav',
+	'/sounds/23.wav',
+	'/sounds/24.wav',
+	'/sounds/25.wav',
+	'/sounds/26.wav',
+	'/sounds/27.wav',
+	'/sounds/28.wav',
+	'/sounds/29.wav',
+	'/sounds/30.wav',
+];
 let rows = 15;
 let cols = 15;
 
@@ -17,9 +45,9 @@ for(let i=0; i<cols; i++) {
 		container.appendChild(circle);
 		circlesArr[i].push(circle);	
 		const sound = document.createElement('audio');
-		sound.classList.add('sound')
-		circle.appendChild(sound)
-		soundsArr[i].push(sound)
+		sound.classList.add('sound');
+		circle.appendChild(sound);
+		soundsArr[i].push(sound);
 	}
 }
 
@@ -28,23 +56,32 @@ circlesArr.forEach((cols, i) => {
 		circle.addEventListener('click', () => {
 			playAudio()
 			growCircles(i, j);
+						
 		});
 	});
 });
 
 function growCircles(i, j) {
+	const bgColor = document.querySelectorAll('.circle')		
 	if(circlesArr[i] && circlesArr[i][j]) {
 		if(!circlesArr[i][j].classList.contains('grow')) {
 			circlesArr[i][j].classList.add('grow');
-			setTimeout(() => {
+			pickedColor = dynamicColors()			
+			bgColor.forEach(color => {				
+				color.style.backgroundColor = pickedColor
+			});
+			setTimeout(() => {				
 				growCircles(i-1, j)
 				growCircles(i+1, j)
 				growCircles(i, j-1)
 				growCircles(i, j+1)
-			}, 100)
+			}, 200)
 			
 			setTimeout(() => {
 				circlesArr[i][j].classList.remove('grow');
+				bgColor.forEach(color => {				
+					color.style.backgroundColor = "#fff"
+				});
 			}, 500);
 		}
 	}
@@ -53,9 +90,16 @@ function growCircles(i, j) {
 function playAudio() {
 	const audio = document.querySelector('.sound')
 	let track = tracks[Math.floor(Math.random() * tracks.length)];
-
 	setTimeout(() => {
 		audio.src = track;
 		audio.play()
 	}, 0)
+}
+
+function dynamicColors() {
+	let r = Math.floor(Math.random() * 255);
+	let g = Math.floor(Math.random() * 255);
+	let b = Math.floor(Math.random() * 255);
+	let color = `rgb(${r}, ${g}, ${b})`
+	return color;
 }
